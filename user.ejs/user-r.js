d179ec54e16e7924020dd4e3fdb8648c72cd9129
YutingLi2001 <90287593+YutingLi2001@users.noreply.github.com>
2021-11-23 09:21:28 +0800

    Delete user.ejs directory
    will deploy the user.ejs into TeaStir

--- user.ejs/user-r.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("LogIn").addEventListener("submit", LogInForm, false);
-document.getElementById("LogIn").addEventListener("reset", ResetForm, false);
-document.getElementById("Register").addEventListener("submit", RegisterForm, false);
-document.getElementById("Register").addEventListener("reset", ResetForm, false);
