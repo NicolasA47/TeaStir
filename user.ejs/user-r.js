7272a32331d514d2d9a7aafb87f192e8c093a11c
YutingLi2001 <90287593+YutingLi2001@users.noreply.github.com>
2021-11-23 09:03:50 +0800

    Add files via upload

---
+++ user.ejs/user-r.js
@@ -0,0 +1,4 @@
+document.getElementById("LogIn").addEventListener("submit", LogInForm, false);
+document.getElementById("LogIn").addEventListener("reset", ResetForm, false);
+document.getElementById("Register").addEventListener("submit", RegisterForm, false);
+document.getElementById("Register").addEventListener("reset", ResetForm, false);
