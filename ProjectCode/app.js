bb8e9284598008382fbe88cda11f38a6e86263c0
Nicolas Ansell <nra862@uregina.ca>
2021-11-24 17:24:35 -0600

    cleaning up code

--- ProjectCode/app.js
+++ ProjectCode/app.js
@@ -3,42 +3,25 @@ const mongoose = require("mongoose");
 const teaFlavour = require('./models/teaFlavour');
 const surveyData = require('./models/surveyData');
 const fs = require("fs");
-const { append } = require("express/lib/response");
 
 
 const app = express();
-const url = 'mongodb://localhost:27017/teaStir';
-const port = 2000; 
+const url = 'mongodb://localhost:27017/teaStir';    //database to be used
+const port = 2000;   //port for server
 
 const teaTypesArray = ["Herbal", "Green", "Black", "White", "Matcha", "Oolong", "Rooibos"];
 const teaFlavoursArray = ["Chai", "Vanilla", "EarlGrey", "Natural", "Pumpkin", "Jasmine", "Mint", "Chocolate", "Stevia-Free", "Organic"];
 const teaCaffeineArray = ["Non-Caffeinated", "Low-Caffeination", "Medium-Caffeination", "High-Caffeination"];
-// let surveyResults = new surveyData({
-//     caffeineLevels: [
-//         {name: "none", rating: 1},{name: "low", rating: 1}, {name: "medium", rating: 3}, {name: "high", rating: 4}
-//     ],
-//     categories: [
-//         {name: "herbal", rating: 3},{name: "green", rating: 3},{name: "black", rating: 3},
-//         {name: "white", rating: 3},{name: "rooibus", rating: 3},{name: "oolong", rating: 3},
-//         {name: "pu'erh", rating: 3},{name: "maté", rating: 3}
-//     ],
-//     flavours:[
-//         {name: "chai", rating: 4},{name: "pumkin", rating: 2},{name: "earl-grey", rating: 1},
-//         {name: "coffee", rating: 0},{name: "chocolate", rating: 3},{name: "berries", rating: 3},
-//         {name: "mint", rating: 4},{name: "vanilla", rating: 2},{name: "natural", rating: 1},
-//         {name: "unflavoured", rating: 0},{name: "stevie-free", rating: 0},{name: "citrus", rating: 2},
-//         {name: "ginger", rating: 4},{name: "hibiscus", rating: 3},{name: "jasmine", rating: 1},
-//     ],
-// });
+//array's used by ejs files for options on the survey questions 
 
 
 mongoose.connect( url, { useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => {
     app.listen (port, () => {
-        console.log (`Server is running on http://localhost:${port}`);
+        console.log (`Server is running on http://localhost:${port}`);  //gives user link to webpage where server is running
     });
 })
-.catch((err) => console.log(err));
+.catch((err) => console.log(err));      //if error is caught console log it
 
 
 app.set("view engine", "ejs");
@@ -48,10 +31,10 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-    res.render('homepage');                 //render the homepage
+    res.render('homepage');                 //render the homepage on load
 });
 
-app.post("/start-survey", (req, res) => {
+app.post("/start-survey", (req, res) => {      //start survey, render the first question with the ejs array data for the questions
     res.render('survey-page', {
     options : teaTypesArray,
     name: "Category"
@@ -59,21 +42,19 @@ app.post("/start-survey", (req, res) => {
 
 });
 
-app.post("/Category", (req, res) => {  /// post from category data
-    let data = JSON.parse(req.body.surveyRatings);
-    const survey = new surveyData(
+
+app.post("/Category", (req, res) => {  /// post from category and create a data object
+    let data = JSON.parse(req.body.surveyRatings);  
+    const survey = new surveyData(      
     {
         caffeineLevels: [],
         categories: data,
         flavours: [],
     });
     
-    try {//write data
-
-        // convert JSON object to a string
+    try {//write data to JSON File
         const newData = JSON.stringify(survey);
     
-        // write file to disk
         fs.writeFileSync('./survey.json', newData, 'utf8');
         console.log(`File is written successfully!`);
     
@@ -87,20 +68,20 @@ app.post("/Category", (req, res) => {  /// post from category data
     });
 });
 
-app.post("/Flavour", (req, res) => {  /// post from flavour data
-    let data = JSON.parse(req.body.surveyRatings);
-    try { //read data
 
-        const JSONdata = fs.readFileSync('./survey.json', 'utf8');
+app.post("/Flavour", (req, res) => {  
+    let data = JSON.parse(req.body.surveyRatings);
+    try { //read data from json file
+
+        const JSONdata = fs.readFileSync('./survey.json', 'utf8');  
         const parsedJSONdata = JSON.parse(JSONdata);
 
         let appendedData = parsedJSONdata;
         appendedData.flavours = data;
 
-        try {//write data
+        try {//write data to JSON File
             const newData = JSON.stringify(appendedData);
-        
-            // write file to disk
+
             fs.writeFileSync('./survey.json', newData, 'utf8');
             console.log(`File is written successfully!`);
         
@@ -112,32 +93,33 @@ app.post("/Flavour", (req, res) => {  /// post from flavour data
         console.log(`Error reading file from disk: ${err}`);
     }
     res.render('survey-page', {
-        options : teaCaffeineArray,
+        options : teaCaffeineArray,     //render survey-page with new data.
         name : "CaffeineLevel"
     });
 });
+
 
 app.post("/CaffeineLevel", (req, res) => {  /// post from caffeine data
     let data = JSON.parse(req.body.surveyRatings);
     try { //read data
 
-        const JSONdata = fs.readFileSync('./survey.json', 'utf8');
+        const JSONdata = fs.readFileSync('./survey.json', 'utf8'); 
         const parsedJSONdata = JSON.parse(JSONdata);
 
         let appendedData = parsedJSONdata;
         appendedData.caffeineLevels = data;
         const survey = new surveyData(
             {
-                caffeineLevels: appendedData.caffeineLevels,
+                caffeineLevels: appendedData.caffeineLevels,    //create a new data object to store the data into mongoose
                 categories: appendedData.categories,
                 flavours: appendedData.flavours,
             });
 
-        survey.save();
+        survey.save();      //save survey data to mongoose 
         teaFlavour.find()                       //search database for teas
         .then((result) =>{     
             
-          calcScores(result, survey, function(scores){
+          calcScores(result, survey, function(scores){      //run calcscores to generate a list of the best teas for the user
               res.render('results', {
                   teaScores: scores,
               }); 
@@ -154,25 +136,25 @@ app.post("/CaffeineLevel", (req, res) => {  /// post from caffeine data
   
 
 
-function calcScores(teaList, surveyData, callback){
+function calcScores(teaList, surveyData, callback){         //take in a list of all the teas and the users survey data
     let caffeineLevels = surveyData.caffeineLevels;
     let flavours = surveyData.flavours;
     let categories = surveyData.categories;
     let scoreArray = [];
-    teaList.forEach(tea => {
+    teaList.forEach(tea => {    
         let cafScore = caffeineLevels.find(level => (level.option === tea.caffeineLevel)).rating;  
         let flavScore = flavours.find(flavour => (flavour.option === tea.flavour)).rating;
         let catScore = categories.find(category => (category.option === tea.category)).rating;
-        let simScore = (flavScore* 9)+(cafScore* 7)+(catScore* 9);
+        let simScore = (flavScore* 9)+(cafScore* 7)+(catScore* 9); //scpre os from (0-4) so each score is multiplied by an amount to determine its value out of 100
         let scoreObj = {
             tea: tea,
             similarityScore: simScore,
-            caffieneScore: cafScore ,
+            caffieneScore: cafScore ,   
             flavourScore: flavScore,
             categoryScore: catScore
         }
         scoreArray.push(scoreObj);
     });
     let sortedArray = scoreArray.sort((a,b) => (b.similarityScore > a.similarityScore) ? 1 : ((a.similarityScore > b.similarityScore) ? -1 : 0));
-    callback(sortedArray);
+    callback(sortedArray);      //callback the now sorted data for ejs
 }
